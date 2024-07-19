c5bb9342d4e2cd7d76e45a43910a1b72d1f0245d
JvFern4ndes <jvfernandes.contato@gmail.com>
2024-07-18 21:26:16 -0300

    feat: transformando os posts em um array

--- contracts/freeSocial.sol
+++ contracts/freeSocial.sol
@@ -3,13 +3,17 @@
 pragma solidity ^0.8.0;
 
 contract freeSocial {
-    mapping(address => string) public posts;
+    mapping(address => string[]) public posts;
 
     function criarPost(string memory _texto) public {
-        posts[msg.sender] = _texto;
+        posts[msg.sender].push(_texto);
     }
 
-    function visualizarPost(address _criadorDoPost) public view returns (string memory) {
+    function visualizarPost(address _criadorDoPost, uint _index) public view returns (string memory) {
+        return posts[_criadorDoPost][_index];
+    }
+
+    function visualizarTodosOsPosts(address _criadorDoPost) public view returns (string[] memory) {
         return posts[_criadorDoPost];
     }
 }
