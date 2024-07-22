69b8b47b9b181aec9d21d2b245ca0048566fb5eb
JvFern4ndes <jvfernandes.contato@gmail.com>
2024-07-22 19:30:17 -0300

    feat: add structs.sol contract

---
+++ contracts/structs.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract RegistroPessoas {
+    
+    struct Pessoa {
+        address id;
+        string nome;
+        uint idade;
+    }
+}
